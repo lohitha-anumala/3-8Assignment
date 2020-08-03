40ccc6c58f9722f582a267ee327f67e89e2f2123
lohitha-anumala <68274764+lohitha-anumala@users.noreply.github.com>
2020-08-03 18:46:39 +0530

    Add files via upload

---
+++ 3-08Assignment/ConsoleAnimal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] MyArray = { "Zebra", "Ant", "Owl", "Lion" };
+            Console.WriteLine("FOr Loop");
+            for(int i = 0; i <MyArray.Length; i++)
+            {
+                Console.Write(MyArray[i]+" ");
+            }
+            Console.WriteLine("For Each Loop");
+            foreach(string i in MyArray)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine("Do while loop  ............");
+            // string i;
+            int j = 0;
+            do
+            {
+                Console.WriteLine( MyArray[j]);
+                j++;
+
+            } while (j<MyArray.Length);
+
+            Console.WriteLine("While Loop...........");
+
+
+            int z = 0;
+            while (z< MyArray.Length)
+            {
+                Console.Write(MyArray[z]+"  ");
+                z++;
+            }
+
+            string[] Animal = { "Zebra", "Kiwi", "Amazona Parrot", "Tiger" };
+
+            string[] Place = { "Africa", "New Zealand", "Jamaica", "India" };
+           Array.Sort(Animal);
+            
+            Console.WriteLine("Displaying Animals in order");
+
+            foreach(string i in Animal)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Displaying place in order");
+            Array.Sort(Place);
+            foreach (string i in Place)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 3-08Assignment/ConsoleStringAssignment/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStringAssignment
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string first;
+            string last;
+            Console.WriteLine("Enter the First String");
+            first = Console.ReadLine();
+            Console.WriteLine("Enter the last string");
+            last = Console.ReadLine();
+           string fullString = first + last;
+            Console.WriteLine("1.Concatination");
+                 Console.WriteLine("2.String Length");
+            Console.WriteLine("3.Substring");
+            Console.WriteLine("4.Replace");
+            Console.WriteLine("5.To Upper");
+            Console.WriteLine("6.To Lower");
+            Console.WriteLine("7.Trim");
+            Console.WriteLine("8.split");
+            Console.WriteLine("Click 9 to exit");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+
+           
+       
+                Console.WriteLine("Select options from 1 to 9");
+
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine(string.Concat(first, last));
+                        break;
+                    case 2:
+                        int length1 = first.Length;
+                        int length2 = last.Length;
+                        Console.WriteLine(length1);
+                        Console.WriteLine(length2);
+                        break;
+
+                    case 3:
+                        string subS = fullString.Substring(5);
+
+                        Console.WriteLine(subS);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter string to replace ");
+                        string newS = Console.ReadLine();
+                        fullString = first + last;
+                        Console.WriteLine(fullString.Replace(last, newS));
+                        break;
+
+                    case 5:
+                        Console.WriteLine(fullString.ToUpper());
+                        break;
+                    case 6:
+                        Console.WriteLine(fullString.ToLower());
+                        break;
+                    case 7:
+                        Console.WriteLine(fullString.Trim());
+                        break;
+
+                    case 8:
+                        Console.WriteLine(fullString.Split(new char[] { 'e' }));
+                        break;
+
+
+
+                }
+
+
+            
+            
+        }
+    }
+}
